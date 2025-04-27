e5699f6db05aaea59811e9268544142443853c0d
Tikhon24 <timihailoff@yandex.ru>
2025-04-27 22:01:31 +0700

    server init

---
+++ main.py
@@ -0,0 +1,10 @@
+# бот
+import requests
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
---
+++ server/server.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from data import db_session
+
+
+app = Flask(__name__)
+
+
+def main():
+    db_session.global_init("db/blogs.db")
+
+
+if __name__ == '__main__':
+    app.run()
+    main()
